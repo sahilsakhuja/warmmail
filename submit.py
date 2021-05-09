05842c7a3c33129c15b23c56da93fdadb1a818e2
sahilsakhuja <sahilsakhuja85@gmail.com>
2021-05-09 16:10:29 +0200

    feat: Added travis and code submission to Canvas [skip ci]

---
+++ submit.py
@@ -0,0 +1,13 @@
+import os
+
+from csci_utils.canvas import CanvasManager
+
+canvas_mgr = CanvasManager(os.environ.get("CANVAS_COURSE"))
+canvas_mgr.csci_utils_updated = False
+canvas_mgr.cookiecutter_updated = False
+canvas_mgr.addl_comments = (
+    "Sphinx documentation link: https://warmmail.readthedocs.io/en/latest/"
+)
+comments = canvas_mgr.pset_submit_assignment("Final Project")
+print("Assignment submitted with comments:")
+print(comments)
