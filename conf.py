dabf45d7489b9c01c81dad63268aef876562da95
sahilsakhuja <sahilsakhuja85@gmail.com>
2021-05-09 12:25:06 +0200

    doc: RSTs and Sphinx conf updated [skip ci]

--- conf.py
+++ conf.py
@@ -10,10 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+import django
+
+sys.path.insert(0, os.path.abspath("."))
+
+# Specify settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+# Setup Django
+
+django.setup()
 
 # -- Project information -----------------------------------------------------
 
